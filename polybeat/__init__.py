9eebc138779988d1689e8502f2675554c3c87ed8
Sneezy132 <nilshendrik13@gmail.com>
2023-03-11 19:56:11 +0100

    updated

--- polybeat/__init__.py
+++ polybeat/__init__.py
@@ -2,18 +2,19 @@ from manim import *
 import numpy as np
 import os
 
-def play(n_edges=[3,4,12], vols=[0,-2,-6]):
+
+def play(n_edges=[1, 12, 4, 3], vols=[0, -2, -6, -10]):
     class Polybeat(Scene):
         def construct(self):
             dr = .16
-            colors = ['#ff7473','#ffc952','#47b8e0']
+            colors = ['#ff7473', '#ffc952', '#47b8e0', '#63e663']
             N = len(n_edges)
             op = .4
             T = 1.8
-            sounds = ['kick_drum','open_conga','low_bongo']
+            sounds = ['kick_drum', 'open_conga', 'low_bongo', 'snare']
             dirname = os.path.dirname(__file__)
-            sounds = [os.path.join(dirname,'sounds',s) for s in sounds]
-            trs = np.linspace(2.5,3.8,N)
+            sounds = [os.path.join(dirname, 'sounds', s) for s in sounds]
+            trs = np.linspace(2.5, 3.8, N)
 
             circs = [
                 Circle(radius=r).rotate(PI/2)
@@ -21,19 +22,20 @@ def play(n_edges=[3,4,12], vols=[0,-2,-6]):
             ]
 
             ps = [
-                [i/n for i in range(n)] 
+                [i/n for i in range(n)]
                 for n in n_edges
             ]
             vs = [
                 [circs[i].point_from_proportion(p) for p in ps[i]]
                 for i in range(N)
             ]
-            geoms = [Polygon(*v).set_stroke(colors[i],14,op) for i,v in enumerate(vs)]
+            geoms = [Polygon(*v).set_stroke(colors[i], 14, op)
+                     for i, v in enumerate(vs)]
 
             dots = []
             for i in range(N):
                 dots.append(
-                    Dot([0,3.5,0], dr*1.5, color=colors[i]).set_opacity(1)
+                    Dot([0, 3.5, 0], dr*1.5, color=colors[i]).set_opacity(1)
                 )
                 dots[-1]._i = i
                 dots[-1]._n = n_edges[i]
@@ -54,7 +56,7 @@ def play(n_edges=[3,4,12], vols=[0,-2,-6]):
                     d.move_to(d._g.point_from_proportion(d._p))
                     next_p = (d._p + dt/T) % 1
                     for p in d._ps:
-                        if (d._p<=p and next_p>=p) or (next_p < d._p):
+                        if (d._p <= p and next_p >= p) or (next_p < d._p):
                             self.add_sound(sounds[d._i], 0, vols[d._i])
                             geoms[d._i].set_stroke(opacity=1)
                             break
@@ -63,15 +65,17 @@ def play(n_edges=[3,4,12], vols=[0,-2,-6]):
                     d.set_opacity(0)
                     d._g.set_stroke(opacity=0)
                     d._p = .9999
-            for d in dots[::-1]: 
+            for d in dots[::-1]:
                 d.add_updater(update_dot)
 
-            def play(i0,i1,i2):
+            def play(i0, i1, i2, i3):
                 dots[i0]._on = True
                 self.wait(T)
                 dots[i1]._on = True
                 self.wait(T*2)
                 dots[i2]._on = True
+                self.wait(T*2)
+                dots[i3]._on = True
 
                 self.wait(T*7)
 
@@ -80,7 +84,9 @@ def play(n_edges=[3,4,12], vols=[0,-2,-6]):
                 dots[i1]._on = False
                 self.wait(T)
                 dots[i2]._on = False
-                dots[i2].update(0)
+                self.wait(T)
+                dots[i3]._on = False
+                dots[i3].update(0)
 
             self.wait(.5)
             self.add(*geoms[::-1], *dots[::-1])
@@ -93,10 +99,16 @@ def play(n_edges=[3,4,12], vols=[0,-2,-6]):
             dots[2]._on = True
             self.wait(T)
             dots[2]._on = False
-            dots[2].update(0)
+            dots[3]._on = True
+            self.wait(T)
+            dots[3]._on = False
+            dots[3].update(0)
 
-            play(2,1,0)
+            play(3, 2, 1, 0)
             self.remove(*geoms[::-1], *dots[::-1])
             self.wait(.5)
     pb = Polybeat()
     pb.render(preview=True)
+
+
+play([16, 12, 8, 4])
